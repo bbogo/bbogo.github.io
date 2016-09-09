2462f9266916cdc62ac67d21d70f19f3079d2ade
bbogo <beni22sof@yahoo.com>
2016-09-10 01:38:43 +0200

    something

---
+++ index_test.php
@@ -0,0 +1,81 @@
+<html>
+  <head>
+   <link rel="stylesheet" href="styles.css">
+   <link rel="icon" href="favicon.png" />
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47560351-1', 'univ-savoie.fr');
+  ga('send', 'pageview');
+
+  </script>
+    <script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script> <script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+});
+</script> <script type="text/javascript" src="path-to-mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+
+    <title>Beniamin Bogosel</title>
+    
+    
+    
+  </head>
+  <body style="   max-width:900px; margin-left:auto ;margin-right:auto">
+   <?php include("header2.html");?>
+
+    
+    <table>
+
+ <!-- columns divs, float left, no margin so there is no space between column, width=1/3 -->
+    <tr><td width = 80%>
+     <!--<h2> <strong>Beniamin Bogosel</strong></h2>-->
+     <br>
+     <strong>PHD student</strong></p>
+    <p> <strong>Coordinators:</strong>&nbsp;<a href="
+    http://www.lama.univ-savoie.fr/%7Ebucur/">Dorin
+        Bucur</a>&nbsp;and <a href="http://www-ljk.imag.fr/membres/Edouard.Oudet/">
+        Édouard Oudet</a></p>
+    <p> <strong>Adress:&nbsp;</strong>Laboratoire de Mathématiques 
+    <a href="http://www.lama.univ-savoie.fr/index.php">LAMA</a>,
+      Université de Savoie, Campus Scientifique, 73 376 Le-Bourget-Du-Lac,
+      France</p>
+    <p> <strong>E-mail:&nbsp;</strong><a href="mailto:
+    beniamin.bogosel@univ-savoie.fr">Beniamin.Bogosel_at_univ-savoie.fr</a></p>
+    <p> <strong>Curriculum Vitae:</strong>  <a href="pdfs/CV-Beni-Bogosel.pdf">
+    pdf version</a></p>
+    <p>
+    <span style="font-weight: bold;">Research Interests: <strong></strong></span>
+    </p>
+    <ul>
+      <li>Calculus of variations, shape optimization </li>
+      <li>Numerical shape optimization: $\Gamma$-convergence methods, relaxation,
+      phase field methods, fundamental solutions  </li>
+      <li>Shape optimization for eigenvalue problems: Dirichlet Laplace eigenvalues, 
+      Steklov/Wentzell Eigenvalues</li>
+      <li>Optimal partitioning problems, multiphase problems</li>
+    </ul>    
+    
+    </td>
+
+    <td width = 20%>
+     <p> <img src="buttons/beni.jpg" width=100% alt=""></p>
+     <p> <div style="text-align: center">Visit 
+     <a href="http://mathproblems123.wordpress.com/">my blog</a></div> </p>
+    </td>
+    </tr>
+</table>
+
+<?php include("header.html");?>
+
+
+
+
+<hr>
+ <p style = "font-size:8px"> Created: Mar 2014, Last modified: Mar 2015</p>
+  </body>
+</html>
